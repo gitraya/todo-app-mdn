c02b544fd773f3c0518e94094547cb011659e98d
gitraya <mail.raya15@gmail.com>
2021-03-17 23:06:55 +0700

    Update

--- src/index.js
+++ src/index.js
@@ -2,10 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+const DATA = [
+  { id: 'todo-0', name: 'Eat', completed: true },
+  { id: 'todo-1', name: 'Sleep', completed: false },
+  { id: 'todo-2', name: 'Repeat', completed: false },
+];
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App tasksData={DATA} />
   </React.StrictMode>,
   document.getElementById('root')
 );
